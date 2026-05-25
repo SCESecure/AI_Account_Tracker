7fb14c8a2ab7a904b5b6f577d355bfca97e54916
SCESecure <harry050327@gmail.com>
2026-05-25 23:52:08 +0900

    홈 화면의 Total 부분 구현 (부분)완료

--- src/components/Body/Home/Total.tsx
+++ src/components/Body/Home/Total.tsx
@@ -1,7 +1,16 @@
 export default function Total() {
+  const date = new Date();
+
   return (
     <>
-      <h3>Hello, Total Component!</h3>
+      <div>
+        <p>총 자산</p>
+        {/* 이 부분은 계좌 총 자산을 계산해서 여기까지 끌어와야 함 */}
+        <p>(전체 자산)원</p>
+        <p>
+          {date.getMonth()}월 {date.getDay()}일 기준
+        </p>
+      </div>
     </>
   );
 }
