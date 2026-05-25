88e321cf40b2366999a8523e165c3e514f8126a7
SCESecure <harry050327@gmail.com>
2026-05-26 00:14:09 +0900

    홈 화면 - 리스트 레이아웃 (부분) 완료

--- src/components/Body/Home/FiveList.tsx
+++ src/components/Body/Home/FiveList.tsx
@@ -2,6 +2,26 @@ export default function FiveList() {
   return (
     <>
       <h3>Hello, FiveList Component!</h3>
+      <div>
+        <p>(리스트 1)</p>
+        <p>(날짜 1)</p>
+        <p>(값 1)</p>
+      </div>
+      <div>
+        <p>(리스트 2)</p>
+        <p>(날짜 2)</p>
+        <p>(값 2)</p>
+      </div>
+      <div>
+        <p>(리스트 3)</p>
+        <p>(날짜 3)</p>
+        <p>(값 3)</p>
+      </div>
+      <div>
+        <p>(리스트 4)</p>
+        <p>(날짜 4)</p>
+        <p>(값 4)</p>
+      </div>
     </>
   );
 }
