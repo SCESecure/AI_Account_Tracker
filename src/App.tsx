8f950653a90913eb09c4a7494818fc1f50b595eb
SCESecure <harry050327@gmail.com>
2026-05-29 20:39:03 +0900

    버튼을 누를때 마다 컴포넌트 보이거나 안보이는 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ export default function App() {
   return (
     <>
       <Header screen={screen} />
-      <Body />
+      <Body screen={screen} />
       <Menu screenHandler={screenHandler} />
     </>
   );
