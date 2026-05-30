bfd6715e5bf0ab7feb735aafc5491923dc2e7a99
SCESecure <harry050327@gmail.com>
2026-05-30 23:35:10 +0900

    화살표 버튼을 누를 때 마다 날짜 변화함

--- src/components/Body/List/Align.tsx
+++ src/components/Body/List/Align.tsx
@@ -1,14 +1,44 @@
+import { useState } from "react";
+
 export default function Align() {
   const date = new Date();
+  const [year, setYear] = useState<number>(date.getFullYear());
+  const [month, setMonth] = useState<number>(date.getMonth() + 1);
+
+  const dateMinusHandler = (): void => {
+    if (month <= 1) {
+      setMonth(12);
+      setYear(year - 1);
+      return;
+    }
+    setMonth(month - 1);
+    return;
+  };
+  const datePlusHandler = (): void => {
+    if (month >= 12) {
+      setMonth(1);
+      setYear(year + 1);
+      return;
+    }
+    setMonth(month + 1);
+    return;
+  };
 
   return (
     <>
       {/* 날짜 부분 */}
-      <button type="button">(왼쪽 화살표)</button>
+      <button type="button" onClick={dateMinusHandler}>
+        (왼쪽 화살표)
+      </button>
       <p>
-        {date.getFullYear()}년 {date.getMonth()}월
+        {year}년 {month}월
       </p>
-      <button>(오른쪽 화살표)</button>
+      <button
+        onClick={datePlusHandler}
+        disabled={month >= date.getMonth() + 1 && year >= date.getFullYear()}
+      >
+        (오른쪽 화살표)
+      </button>
 
       <button>최신순</button>
       <button>(오름차순)</button>
