972d2fb6ab1986d4cb9e8916f63d52dc4c762080
SCESecure <harry050327@gmail.com>
2026-05-31 02:17:57 +0900

    상태 끌어올리기 추가 그리고 정렬 종류를 금액순 추가함

--- src/components/Body/List/Align.tsx
+++ src/components/Body/List/Align.tsx
@@ -1,47 +1,24 @@
-import { useState } from "react";
-
-export default function Align() {
-  const date = new Date();
-  const [year, setYear] = useState<number>(date.getFullYear());
-  const [month, setMonth] = useState<number>(date.getMonth() + 1);
-  const [order, setOrder] = useState<string>("recentOrder");
-  const [isAscend, setIsAscend] = useState<boolean>(false);
-
-  const dateMinusHandler = (): void => {
-    if (month <= 1) {
-      setMonth(12);
-      setYear(year - 1);
-      return;
-    }
-    setMonth(month - 1);
-    return;
-  };
-  const datePlusHandler = (): void => {
-    if (month >= 12) {
-      setMonth(1);
-      setYear(year + 1);
-      return;
-    }
-    setMonth(month + 1);
-    return;
-  };
-  const orderHandler = (): void => {
-    if (order === "recentOrder") {
-      setOrder("oldOrder");
-      return;
-    }
-    setOrder("recentOrder");
-  };
-
-  const isAscendHandler = (): void => {
-    if (isAscend) {
-      setIsAscend(false);
-      return;
-    }
-    setIsAscend(true);
-    return;
-  };
-
+export default function Align({
+  tyear,
+  tmonth,
+  order,
+  isAscend,
+  tdate,
+  orderHandler,
+  isAscendHandler,
+  dateMinusHandler,
+  datePlusHandler,
+}: {
+  tyear: number;
+  tmonth: number;
+  tdate: Date;
+  order: string;
+  isAscend: boolean;
+  orderHandler: () => void;
+  isAscendHandler: () => void;
+  dateMinusHandler: () => void;
+  datePlusHandler: () => void;
+}) {
   return (
     <>
       {/* 날짜 부분 */}
@@ -49,17 +26,19 @@ export default function Align() {
         (왼쪽 화살표)
       </button>
       <p>
-        {year}년 {month}월
+        {tyear}년 {tmonth}월
       </p>
       <button
         onClick={datePlusHandler}
-        disabled={month >= date.getMonth() + 1 && year >= date.getFullYear()}
+        disabled={
+          tmonth >= tdate.getMonth() + 1 && tyear >= tdate.getFullYear()
+        }
       >
         (오른쪽 화살표)
       </button>
 
       <button onClick={orderHandler}>
-        {order === "recentOrder" ? "최신순" : "오래된순"}
+        {order === "recentOrder" ? "최신순" : "가격순"}
       </button>
       <button onClick={isAscendHandler}>
         {isAscend ? "(오름차순)" : "(내림차순)"}
--- src/components/Body/List/List.tsx
+++ src/components/Body/List/List.tsx
@@ -7,46 +7,62 @@ import { v4 as uuidv4 } from "uuid";
 
 export interface List {
   id: string;
-  date: string;
+  month: number;
+  day: number;
+  compareDay: number; // 이거는 정렬을 할 때 쓰는거
   category: string;
   isExpense: boolean; // income일 경우 false, 아닐 경우 true
   price: number;
 }
 
+// 이거 나중에 input 컴포넌트 만들때 반드시 업뎃해야 함!!!!
+// 즉, 임시 값임!
 const date = new Date();
+const month = date.getMonth() + 1;
+const day = date.getDate();
 
 const defaultList: List[] = [
   {
     id: uuidv4(),
-    date: date.getMonth() + "월 " + date.getDay() + "일",
+    month: month,
+    day: day,
+    compareDay: month * 30 + day,
     category: "급여",
     isExpense: false,
     price: 2000000,
   },
   {
     id: uuidv4(),
-    date: date.getMonth() + "월 " + date.getDay() + "일",
+    month: month,
+    day: day,
+    compareDay: month * 30 + day,
     category: "용돈",
     isExpense: false,
     price: 30000,
   },
   {
     id: uuidv4(),
-    date: date.getMonth() + "월 " + date.getDay() + "일",
+    month: month,
+    day: day,
+    compareDay: month * 30 + day,
     category: "식비",
     isExpense: true,
     price: 15000,
   },
   {
     id: uuidv4(),
-    date: date.getMonth() + "월 " + date.getDay() + "일",
+    month: month,
+    day: day,
+    compareDay: month * 30 + day,
     category: "교통비",
     isExpense: true,
     price: 3000,
   },
   {
     id: uuidv4(),
-    date: date.getMonth() + "월 " + date.getDay() + "일",
+    month: month,
+    day: day,
+    compareDay: month * 30 + day,
     category: "문화생활",
     isExpense: true,
     price: 20000,
@@ -54,11 +70,82 @@ const defaultList: List[] = [
 ];
 
 export default function List() {
+  const tdate = new Date();
+
   const [list, setList] = useState<List[]>(defaultList);
+  const [order, setOrder] = useState<string>("recentOrder");
+  const [isAscend, setIsAscend] = useState<boolean>(false);
+  const [tyear, setTyear] = useState<number>(tdate.getFullYear());
+  const [tmonth, setTmonth] = useState<number>(tdate.getMonth() + 1);
+
+  // -- 핸들러 영역 --
+
+  // 정렬 종류는 "최신순" 또는 "가격순"임
+  const orderHandler = (): void => {
+    if (order === "recentOrder") {
+      setOrder("priceOrder");
+      return;
+    }
+    setOrder("recentOrder");
+  };
+
+  // 정렬 방식은 두 종류에 따른 오름차순/내림차순
+  const isAscendHandler = (): void => {
+    // 최신순일 때
+    if (order === "recentOrder") {
+      if (isAscend) {
+        setList(list.sort((a, b) => a.compareDay - b.compareDay));
+        setIsAscend(false);
+        return;
+      }
+      setList(list.sort((a, b) => b.compareDay - a.compareDay));
+      setIsAscend(true);
+      return;
+    }
+
+    // 금액순일 때
+    if (isAscend) {
+      setList(list.sort((a, b) => a.price - b.price));
+      setIsAscend(false);
+      return;
+    }
+    setList(list.sort((a, b) => b.price - a.price));
+    setIsAscend(true);
+    return;
+  };
+
+  const dateMinusHandler = (): void => {
+    if (tmonth <= 1) {
+      setTmonth(12);
+      setTyear(tyear - 1);
+      return;
+    }
+    setTmonth(tmonth - 1);
+    return;
+  };
+  const datePlusHandler = (): void => {
+    if (month >= 12) {
+      setTmonth(1);
+      setTyear(tyear + 1);
+      return;
+    }
+    setTmonth(tmonth + 1);
+    return;
+  };
 
   return (
     <>
-      <Align />
+      <Align
+        tyear={tyear}
+        tmonth={tmonth}
+        tdate={tdate}
+        order={order}
+        isAscend={isAscend}
+        orderHandler={orderHandler}
+        isAscendHandler={isAscendHandler}
+        dateMinusHandler={dateMinusHandler}
+        datePlusHandler={datePlusHandler}
+      />
       <AllList list={list} />
     </>
   );
