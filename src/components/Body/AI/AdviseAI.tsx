ef777b0d60beb3f88e4a119b2838d510469e48fe
SCESecure <harry050327@gmail.com>
2026-06-03 10:51:04 +0900

    AI.tsx 기본 레이아웃 생성 및 .env 생성

--- src/components/Body/AI/AdviseAI.tsx
+++ src/components/Body/AI/AdviseAI.tsx
@@ -1,11 +1,25 @@
 import AdviseItem from "./AdviseItem";
 
 export default function AdviseAI() {
+  const todayDate = new Date();
+  const adviseItem = [];
+
+  for (let i = 0; i < 3; i++) {
+    adviseItem.push(
+      <li>
+        <AdviseItem />
+      </li>,
+    );
+  }
+
   return (
     <>
-      <h3>Hello, AdviseAI Component!</h3>
+      {/* 여기 가운데 정렬 필요! */}
+      <h3>
+        AI가 분석한 <br /> {todayDate.getMonth() + 1}월 소비 트렌드에요!
+      </h3>
 
-      <AdviseItem />
+      <ul>{adviseItem}</ul>
     </>
   );
 }
