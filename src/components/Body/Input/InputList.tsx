f1db07590c2cd78c59eb14c141bc9112b8547eb8
SCESecure <harry050327@gmail.com>
2026-06-10 15:10:48 +0900

    수입, 지출 버튼 생성

--- src/components/Body/Input/InputList.tsx
+++ src/components/Body/Input/InputList.tsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
 import CategoryInput from "./CategoryInput";
 import ExpenseInput from "./ExpenseInput";
 import IncomeInput from "./IncomeInput";
 
 export default function InputList() {
+  const [isIncome, setIsIncome] = useState<boolean>(true);
+
+  const handleIsIncome = (): void => {
+    if (!isIncome) {
+      console.log("Income Mode");
+      setIsIncome(true);
+      return;
+    }
+    console.log("Expense Mode");
+    setIsIncome(false);
+    return;
+  };
+
   return (
     <>
       <h3>Hello, InputList Component!</h3>
+
+      <button onClick={handleIsIncome} disabled={isIncome}>
+        수입
+      </button>
+      <button onClick={handleIsIncome} disabled={!isIncome}>
+        지출
+      </button>
 
       <CategoryInput />
       <IncomeInput />
